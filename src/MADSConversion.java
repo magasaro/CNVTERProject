7d4a7976b20eed3e68eec1b4a9266965a9bc8338
doran <doransanders9@gmail.com>
2024-02-14 14:48:37 -0600

    Initial Commit

--- src/MADSConversion.java
+++ src/MADSConversion.java
@@ -1,8 +1,8 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
+public class MADSConversion {
     public static void main(String[] args) {
+
 
         }
     }
-}
